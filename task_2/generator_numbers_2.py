aa517d521e5b2fa2c1d3fb02c2aa9bb1312180ae
Petro Davydov <arturshopengauergik@gmail.com>
2025-03-17 12:36:04 +0200

    add task_2 generator_numbers

---
+++ task_2/generator_numbers_2.py
@@ -0,0 +1,19 @@
+import re
+# from typing import Callable # працює і без цього, але callable з маленької букви
+
+
+def generator_numbers(text: str) -> any:
+    pattern = r"\b\d+\.\d+\b"  # регулярний вираз для знаходження чисел
+    # тут також можливе використання re.finditter()
+    for match in re.findall(pattern, text):
+        # повернення числа як результату;можливе группування типу: yield float(match.groupe())
+        yield float(match)
+
+
+def sum_profit(text: str, func: callable):
+    return sum(func(text))
+
+
+text = "Загальний дохід працівника складається з декількох частин: 1000.01 як основний дохід, доповнений додатковими надходженнями 27.45 і 324.00 доларів."
+total_income = sum_profit(text, generator_numbers)
+print(f"Загальний дохід: {total_income}")
