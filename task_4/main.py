f3a6fe3be1e827e13d6319f777765b92bc180fb8
Petro Davydov <arturshopengauergik@gmail.com>
2025-03-17 15:48:30 +0200

    change CMD_COMMAND

--- task_4/main.py
+++ task_4/main.py
@@ -1,6 +1,14 @@
 from managed_contacts import *
 
 
+CMD_CLOSE = {"close", "exit"}
+CMD_HELLO = {"hello"}
+CMD_ALL = {"all"}
+CMD_ADD = {"add"}
+CMD_CHANGE = {"change"}
+CMD_PHONE = {"phone"}
+
+
 def parse_input(user_input):
     cmd, *args = user_input.split()
     cmd = cmd.strip().lower()
@@ -14,18 +22,18 @@ def main():
         user_input = input("Enter a command: ")
         command, args = parse_input(user_input)
 
-        if command in ["close", "exit"]:
+        if command in CMD_CLOSE:
             print("Good bye!")
             break
-        elif command == "hello":
+        elif command in CMD_HELLO:
             print("How can I help you?")
-        elif command == "add":
+        elif command in CMD_ADD:
             print(add_contact(args, contacts))
-        elif command == "change":
+        elif command in CMD_CHANGE:
             print(change_contact(args, contacts))
         elif command == "phone":
             print(show_phone(args, contacts))
-        elif command == "all":
+        elif command in CMD_ALL:
             print(show_all(contacts))
         else:
             print("Invalid command.")
